9a2be13e05dd127fcfad97fabe159f0627b45d3f
Joao Andre <jandrevl@gmail.com>
2021-07-18 23:47:42 +0100

    Filled the Contact Class with Constructor, Getters and Setters. The constructor parameter is a Lead Object, and I used the getters and setters I think that object will have. It can be changed later.

--- src/main/java/com/thecleancoders/crm/classes/Contact.java
+++ src/main/java/com/thecleancoders/crm/classes/Contact.java
@@ -1,4 +1,40 @@
 package com.thecleancoders.crm.classes;
 
 public class Contact {
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+    private Lead lead;
+
+    public Contact(Lead lead) {
+        // The new Contact object will have the same name, phoneNumber and emailAddress as the Lead from which
+        // it comes to existence, using it's getters
+        this.name = lead.getName();
+        this.phoneNumber = lead.getPhoneNumber();
+        this.emailAddress = lead.getEmailAddress();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
--- src/main/java/com/thecleancoders/crm/main/Main.java
+++ src/main/java/com/thecleancoders/crm/main/Main.java
@@ -1,7 +1,28 @@
 package com.thecleancoders.crm.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //Initially, we could create an ArrayList with all leads, to be possible to work with.
+        //Later, when we want to close the whole application, by inserting the command "exit", we could
+        // make the program transfer everything in the Leads leadArray and any other arrays (like contactArray,
+        // opportunityArray, etc) into the storage CSV files.
+        Scanner scanner = new Scanner(new File("leads.csv"));
+        ArrayList<String[]> leadsArray = new ArrayList<String[]>();
+
+        while (scanner.hasNextLine()) {
+        String[] leadLine = scanner.nextLine().split(",");
+        leadsArray.add(leadLine);
+        }
+        scanner.close();
+
+        //Just to check
+        System.out.println(leadsArray.get(2)[1]); //should return "Ned Flanders"
 
     }
 }
