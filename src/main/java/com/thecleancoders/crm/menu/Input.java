3aa3cfb3aaa186e77ed0439e1c2492d219488f97
Katarzyna Wasik <kat.wasik@hotmail.com>
2021-07-19 12:28:23 +0200

    Add getObjectTypeFromString methods with tests

--- src/main/java/com/thecleancoders/crm/menu/Input.java
+++ src/main/java/com/thecleancoders/crm/menu/Input.java
@@ -1,15 +1,37 @@
 package com.thecleancoders.crm.menu;
 
 import com.thecleancoders.crm.enums.Command;
+import com.thecleancoders.crm.enums.ObjectTypes;
 
 public class Input {
     // method to convert case insensitive string to Command Enum
     public Command getCommandFromString(String string) {
         string = string.toUpperCase();
-        for (Command command : Command.values())
+        for (Command command : Command.values()) {
             if (string.equals(command.getExpectedInput())) {
                 return command;
             }
+        }
+        return null;
+    }
+
+    public ObjectTypes getObjectTypeFromStringSingular(String string) {
+        string = string.toUpperCase();
+        for (ObjectTypes objectType : ObjectTypes.values()) {
+            if (string.equals(objectType.getSingularForm())) {
+                return objectType;
+            }
+        }
+        return null;
+    }
+
+    public ObjectTypes getObjectTypeFromStringPlural(String string) {
+        string = string.toUpperCase();
+        for (ObjectTypes objectType : ObjectTypes.values()) {
+            if (string.equals(objectType.getPluralForm())) {
+                return objectType;
+            }
+        }
         return null;
     }
 }
--- src/test/java/com/thecleancoders/crm/menu/InputTest.java
+++ src/test/java/com/thecleancoders/crm/menu/InputTest.java
@@ -1,6 +1,7 @@
 package com.thecleancoders.crm.menu;
 
 import com.thecleancoders.crm.enums.Command;
+import com.thecleancoders.crm.enums.ObjectTypes;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +20,19 @@ class InputTest {
         assertEquals(Command.SHOW, input.getCommandFromString("SHOW"));
         assertEquals(Command.SHOW, input.getCommandFromString("show"));
         assertNull(input.getCommandFromString("abc"));
+    }
+
+    @Test
+    void getObjectTypeFromStringSingular() {
+        assertEquals(ObjectTypes.ACCOUNT, input.getObjectTypeFromStringSingular("ACCOUNT"));
+        assertEquals(ObjectTypes.ACCOUNT, input.getObjectTypeFromStringSingular("account"));
+        assertNull(input.getObjectTypeFromStringSingular("abc"));
+    }
+
+    @Test
+    void getObjectTypeFromStringPlural() {
+        assertEquals(ObjectTypes.ACCOUNT, input.getObjectTypeFromStringPlural("ACCOUNTS"));
+        assertEquals(ObjectTypes.ACCOUNT, input.getObjectTypeFromStringPlural("accounts"));
+        assertNull(input.getObjectTypeFromStringPlural("abc"));
     }
 }
