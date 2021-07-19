0afa959443a85f318258e5515cacdb3ed9451dc3
Joao Andre <jandrevl@gmail.com>
2021-07-19 20:04:47 +0100

    Some more changes to Contact Class.Id method to be reviewed

--- src/main/java/com/thecleancoders/crm/classes/Contact.java
+++ src/main/java/com/thecleancoders/crm/classes/Contact.java
@@ -1,17 +1,36 @@
 package com.thecleancoders.crm.classes;
 
+import com.thecleancoders.crm.main.Main;
+
 public class Contact {
+    private int id;
     private String name;
     private String phoneNumber;
-    private String emailAddress;
+    private String email;
     private Lead lead;
 
     public Contact(Lead lead) {
+        //The id must be an increment of the last one created. Let's assume there is an ArrayList
+        //of Contact Objects.So, the id of this object will be the id of the last object of the array
+        // plus one.
+        if (Main.contactsList.size() < 1) {
+            this.id = 0;
+        } else {
+            this.id = Main.contactsList.get(Main.contactsList.size() - 1).getId() + 1;
+        }
         // The new Contact object will have the same name, phoneNumber and emailAddress as the Lead from which
         // it comes to existence, using it's getters
         this.name = lead.getName();
         this.phoneNumber = lead.getPhoneNumber();
-        this.emailAddress = lead.getEmailAddress();
+        this.email = lead.getEmail();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -30,11 +49,13 @@ public class Contact {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getEmailAddress() {
-        return emailAddress;
+    public String getEmail() {
+        return email;
     }
 
     public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
+        this.email = email;
     }
+
 }
+
--- src/main/java/com/thecleancoders/crm/main/Main.java
+++ src/main/java/com/thecleancoders/crm/main/Main.java
@@ -1,28 +1,28 @@
 package com.thecleancoders.crm.main;
 
+import com.thecleancoders.crm.classes.Contact;
+import com.thecleancoders.crm.classes.Lead;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    //Initially, we could create an ArrayList with all leads, to be possible to work with.
+    //Later, when we want to close the whole application, by inserting the command "exit", we could
+    // make the program transfer everything in the Leads leadArray and any other arrays (like contactArray,
+    // opportunityArray, etc) into the storage CSV files.
+    public static ArrayList<Lead> leadsList = new ArrayList<Lead>();
+    public static ArrayList<Contact> contactsList = new ArrayList<Contact>();
+
     public static void main(String[] args) throws FileNotFoundException {
 
-        //Initially, we could create an ArrayList with all leads, to be possible to work with.
-        //Later, when we want to close the whole application, by inserting the command "exit", we could
-        // make the program transfer everything in the Leads leadArray and any other arrays (like contactArray,
-        // opportunityArray, etc) into the storage CSV files.
-        Scanner scanner = new Scanner(new File("leads.csv"));
-        ArrayList<String[]> leadsArray = new ArrayList<String[]>();
 
-        while (scanner.hasNextLine()) {
-        String[] leadLine = scanner.nextLine().split(",");
-        leadsArray.add(leadLine);
-        }
-        scanner.close();
+        
 
-        //Just to check
-        System.out.println(leadsArray.get(2)[1]); //should return "Ned Flanders"
+
+       
 
     }
 }
