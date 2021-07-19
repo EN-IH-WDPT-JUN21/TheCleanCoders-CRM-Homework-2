050d05bb660bf5061d8a972ded8d7d999d1b44e3
Katarzyna Wasik <kat.wasik@hotmail.com>
2021-07-19 11:53:28 +0200

    Add Command Enum

---
+++ src/main/java/com/thecleancoders/crm/enums/Command.java
@@ -0,0 +1,22 @@
+package com.thecleancoders.crm.enums;
+
+public enum Command {
+    NEW("NEW"),
+    SHOW("SHOW"),
+    LOOKUP("LOOKUP"),
+    CONVERT("CONVERT"),
+    CLOSE_LOST("CLOSE-LOST"),
+    CLOSE_WON("CLOSE-WON"),
+    CLOSE_OPEN("CLOSE-OPEN"),
+    EXIT("EXIT");
+
+    private String expectedInput;
+
+    Command(String expectedInput) {
+        this.expectedInput = expectedInput;
+    }
+
+    public String getExpectedInput() {
+        return expectedInput;
+    }
+}
