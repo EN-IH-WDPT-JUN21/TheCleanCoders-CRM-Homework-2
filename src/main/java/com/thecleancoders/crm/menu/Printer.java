5057a399827abbc3ff3095bf4e5970a09abbf516
Joao Andre <jandrevl@gmail.com>
2021-07-28 21:52:21 +0100

    printAll methods in Printer

--- src/main/java/com/thecleancoders/crm/menu/Printer.java
+++ src/main/java/com/thecleancoders/crm/menu/Printer.java
@@ -1,25 +1,34 @@
 package com.thecleancoders.crm.menu;
 
+import com.thecleancoders.crm.classes.*;
+
 public class Printer {
     public void print(String text) {
         System.out.println(text);
     }
 
     public void printAllAccounts() {
-
-
+        for (Item account : Account.allAccounts) {
+            print(account.toString());
+        }
     }
 
     public void printAllContacts() {
-
+        for (Item contact : Contact.allContacts) {
+            print(contact.toString());
+        }
     }
 
     public void printAllLeads() {
-
+        for(Item lead : Lead.allLeads) {
+            print(lead.toString());
+        }
     }
 
     public void printAllOpportunities() {
-
+        for (Item opportunity : Opportunity.allOpportunities) {
+            print(opportunity.toString());
+        }
     }
 
     public void printTypoInfo(String wrongWord) {
