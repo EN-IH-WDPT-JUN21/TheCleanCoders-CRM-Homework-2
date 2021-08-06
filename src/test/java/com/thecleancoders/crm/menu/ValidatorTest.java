9f80a2c39b7fb76e8c3b82666c67fc63234beb9e
NatNorb <natalia.n@op.pl>
2021-08-07 00:20:59 +0200

    some more tests (Validator, Printer, Enums)

---
+++ src/test/java/com/thecleancoders/crm/menu/ValidatorTest.java
@@ -0,0 +1,53 @@
+package com.thecleancoders.crm.menu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorTest {
+
+    @Test
+    void isPhoneNumberValidTest() {
+        assertTrue(Validator.isPhoneNumberValid("123-456-789"));
+        assertTrue(Validator.isPhoneNumberValid("666999555"));
+        assertTrue(Validator.isPhoneNumberValid("444.222.333"));
+
+        assertThrows(IllegalArgumentException.class, () -> Validator.isPhoneNumberValid("+48"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isPhoneNumberValid("464648603789123"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isPhoneNumberValid("abc"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isPhoneNumberValid(""));
+    }
+
+    @Test
+    void isEmailValidTest() {
+        assertTrue(Validator.isEmailValid("abcdhhd@defhd.com"));
+        assertTrue(Validator.isEmailValid("shdog@kj.de"));
+        assertTrue(Validator.isEmailValid("koziolek@matolek.pl"));
+
+        assertThrows(IllegalArgumentException.class, () -> Validator.isEmailValid("kjhajhfgi.iaugh"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isEmailValid("hdhdhdh@uhgab"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isEmailValid("abcfgdhah"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isEmailValid("abcfg@dhah.sgs"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isEmailValid(""));
+
+    }
+
+    @Test
+    void isStringValidTest() {
+        assertTrue(Validator.isStringValid("abcdhhddefhdcom"));
+        assertTrue(Validator.isStringValid("shdogkjde"));
+        assertTrue(Validator.isStringValid("qwertyuiopasdfghjklzxcvbnmqwertyui"));
+        assertTrue(Validator.isStringValid("aa"));
+
+        assertThrows(IllegalArgumentException.class, () -> Validator.isStringValid("kjhajhfgi.iaugh"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isStringValid("hdhdhdh@uhgab"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isStringValid("ab cfg dh ah"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isStringValid(""));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isStringValid("a"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isStringValid("1"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isStringValid("."));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isStringValid("!@#$%^&*()"));
+        assertThrows(IllegalArgumentException.class, () -> Validator.isStringValid(" "));
+
+    }
+}
